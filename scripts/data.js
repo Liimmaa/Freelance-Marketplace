2fc01c1e0fffa50bec93500cdf322b71653ce203
Liimmaa <halimammegwa@gmail.com>
2019-10-16 03:23:04 +0100

    linked signup js to sign up page

---
+++ scripts/data.js
@@ -0,0 +1,26 @@
+/*let info = JSON.parse('{"username": "lima" ,"password": 12345}');
+console.log(info);*/
+{
+    "signup": [
+      {
+        "id": 1,
+        "email": "chioma@gmail.com",
+        "password": "12"
+      },
+      {
+        "email": "chioma@gmail.com",
+        "password": "12345",
+        "id": 2
+      },
+      {
+        "email": "halimammegwa@gmail.com",
+        "password": "j,sdhk",
+        "id": 3
+      },
+      {
+        "email": "halimammegwa@gmail.com",
+        "password": "jkwekj",
+        "id": 4
+      }
+    ]
+  }
---
+++ scripts/signup.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#signup").click(function(){
+        let user = $("user").val();
+        let password = $("password").val();
+
+        if (name == '' || password == ''){
+            alert("Please fill in all fields..!!!");
+        }else if (!(password).match())
+    })
+})
